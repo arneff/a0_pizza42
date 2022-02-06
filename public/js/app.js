d10b25a3abc15a699fada50b0858c8ca88669152
Aaron Neff <aaron.neff@stackhawk.com>
2022-02-05 23:44:36 -0600

    call external api

--- public/js/app.js
+++ public/js/app.js
@@ -5,10 +5,11 @@ const fetchAuthConfig = () => fetch("/auth_config.json");
 const configureClient = async () => {
     const response = await fetchAuthConfig();
     const config = await response.json();
-  
+
     auth0 = await createAuth0Client({
-      domain: config.domain,
-      client_id: config.clientId
+        domain: config.domain,
+        client_id: config.clientId,
+        audience: config.audience
     });
   };
 
@@ -48,14 +49,12 @@ const isAuthenticated = await auth0.isAuthenticated();
 if (isAuthenticated) {
     const user = await auth0.getUser()
     document.getElementById("btn-menu-div").classList.remove("hidden");
+    document.getElementById('dropdownMenuButton1').innerHTML = user.name;
     document.getElementById("btn-login-div").classList.add("hidden");
-    document.getElementById("gated-content").classList.remove("hidden");
-
     
 
 } else {
     document.getElementById("btn-login-div").classList.remove("hidden");
-    document.getElementById("gated-content").classList.add("hidden");
 }
 
 
@@ -76,7 +75,56 @@ auth0.logout({
 });
 };
 
-const profile = () => {
-
+const profile = async () => {
+    const user = await auth0.getUser()
+    console.log(user)
+    document.getElementById("pizza-content").classList.add("hidden");
+    document.getElementById("profile-content").classList.remove("hidden");
+    document.getElementById("user.nickname").innerHTML = user.name;
+    document.getElementById("user.email").innerHTML = user.email;
+    document.getElementById("user.email_verified").innerHTML = user.email_verified;
+    document.getElementById("user.metadata").innerHTML = "";
 }
 
+const home = () => {
+    document.getElementById("pizza-content").classList.remove("hidden");
+    document.getElementById("profile-content").classList.add("hidden");
+}
+
+const callApi = async () => {
+    try {
+  
+      // Get the access token from the Auth0 client
+      const token = await auth0.getTokenSilently();
+  
+      // Make the call to the API, setting the token
+      // in the Authorization header
+      const response = await fetch("http://localhost:3001/api/external", {
+        headers: {
+          "Authorization": `Bearer ${token}`,
+
+        }
+      });
+  
+      // Fetch the JSON result
+      responseStatus = response.status
+      if (responseStatus > 200) {
+        responseData = { msg: "Insufficent Scope: a scope of read:users is required" }
+      } else {
+        responseData = await response.json();
+        console.log(responseData)
+      }
+      
+  
+    //   Display the result in the output element
+      const responseElement = document.getElementById("api-call-result");
+      responseElement.innerText = JSON.stringify(responseData, {}, 2);
+
+      
+  
+  } catch (e) {
+      // Display errors in the console
+      console.error(e)
+    }
+  };
+  
--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const { join } = require("path");
+const authConfig = require("./auth_config.json");
+const { requiredScopes } = require('express-oauth2-jwt-bearer');
+const { auth } = require("express-oauth2-jwt-bearer");
+
+
 const app = express();
+const api = express();
+
+const checkJwt = auth({
+  audience: authConfig.audience,
+  issuerBaseURL: `https://${authConfig.domain}`
+});
+
+const checkScopes = requiredScopes('read:users');
 
 // Serve static assets from the /public folder
 app.use(express.static(join(__dirname, "public")));
+api.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', '*');
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'authorization');
+  next();
+});
 
 // Endpoint to serve the configuration file
 app.get("/auth_config.json", (req, res) => {
@@ -15,5 +34,21 @@ app.get("/*", (_, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
+
+// protect this route from unauthorized requests
+api.get("/api/external", checkJwt, checkScopes, (req, res) => {
+  res.send({msg: "Your access token was successfully validated!"});
+});
+
+api.use(function(err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    return res.status(401).send({ msg: "Invalid token" });
+  }
+
+  next(err, req, res);
+});
+
+
 // Listen on port 3000
 app.listen(3000, () => console.log("Application running on port 3000"));
+api.listen(3001, () => console.log("Application running on port 3001"));
