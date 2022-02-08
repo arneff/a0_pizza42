676460b07fe6cde975fad56f98d934d2b182897e
Aaron Neff <aaron.neff@stackhawk.com>
2022-02-08 02:41:16 -0600

    add build folder

---
+++ build/precache-manifest.c10d29a4da85e8caaed10bc14b5a0269.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c5d8824fc7c411143c45c1c7236d83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0729936fdf1b9a2f8833",
+    "url": "/static/css/2.11b452ed.chunk.css"
+  },
+  {
+    "revision": "b3514de6e3504bb0963d",
+    "url": "/static/css/main.1aa3f63c.chunk.css"
+  },
+  {
+    "revision": "0729936fdf1b9a2f8833",
+    "url": "/static/js/2.084d4eef.chunk.js"
+  },
+  {
+    "revision": "24cba0c88f46204e4e9505858b825c5d",
+    "url": "/static/js/2.084d4eef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3514de6e3504bb0963d",
+    "url": "/static/js/main.0b02b0d6.chunk.js"
+  },
+  {
+    "revision": "0231f95ac667b73c9187",
+    "url": "/static/js/runtime-main.91e21c86.js"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "dabc05847775c756c1c271293ea3371a",
+    "url": "/static/media/pizza.dabc0584.png"
+  },
+  {
+    "revision": "3ec930d230b1018f15e005cc35863d33",
+    "url": "/static/media/whole.3ec930d2.png"
+  }
+]);
